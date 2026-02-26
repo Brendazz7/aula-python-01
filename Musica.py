4680a4505170da0b497cd1e55b471d587475fabb
Brenda Mendes Facela <1061000@senacminas.edu.br>
2026-02-26 11:52:25 -0300

    Criação de novos arquivos

---
+++ Musica.py
@@ -0,0 +1,11 @@
+musica = """"
+If you want me to show you
+How happy we could be, I'll be your fool
+You seem so surprised
+You ever drink my love and love is cruel
+If you want me to show you
+How happy we could be, I'll be your fool
+You seem so surprised
+You ever drink my love and love is cruel
+"""
+print(musica)
---
+++ aula1.py
@@ -0,0 +1,8 @@
+#COMEÇO DO CODIGO :]
+from tkinter.ttk import Notebook
+
+
+produto = "Notebook"
+quantidade = 2
+preço_unitário = 3500.52
+print (f"a compra do seu {produto} foi realizada com sucesso, sendo a quantidade {quantidade} e o valor total {(preço_unitário * quantidade): .1f}")
